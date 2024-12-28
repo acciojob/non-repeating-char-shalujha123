f3a3cc1411ec71dd1ffffbb33230c2129c0a6481
Shalu <88194182+shalujha123@users.noreply.github.com>
2024-12-28 18:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = str.split("")
+	let count = {}
+	let flag = false
+	for(let t of arr){
+		if(count[t]){
+			count[t]++
+		}else{
+			count[t] = 1
+			flag = true
+		}
+	}
+	if(flag){
+		for(let t of arr){
+			if(count[t] === 1){
+				return t
+			}
+		}
+	}
+	return null
+	
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
